7ccdaa995a9d15b3143fdc23ebceab96611db006
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-11-06 16:39:27 +0530

    Add files via upload

---
+++ number of tables.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number"))
+a=int(input('eter the value'))
+c=int(input('enter range'))
+while n<=a:
+    i=1
+    while i<=c:
+        print(n,'x',i,'=',n*i)
+        i += 1
+    print()
+    n += 1
+
+    
+    
