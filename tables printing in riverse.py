a56e7d20cedeecc6f52d5730d75f402c02117684
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-11-06 16:54:01 +0530

    Add files via upload

---
+++ tables printing in riverse.py
@@ -0,0 +1,12 @@
+a=int(input("enter the number"))
+b=int(input('eter the value'))
+while b>=a:
+    i=12
+    while i>=1:
+        print(b,'x',i,'=',i*b)
+        i -= 1
+    print()
+    b -= 1
+
+    
+    
