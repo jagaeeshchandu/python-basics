069176900f66c33ea91a468a8a10528d72393842
jagaeeshchandu <92150299+jagaeeshchandu@users.noreply.github.com>
2021-11-06 17:20:10 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,14 @@
+a=int(input("enter the number"))
+b=int(input('eter the value'))
+while a<=b:
+    i=1
+    print(a, end = ' ')
+    c=int(input(" table up to?? "))
+    while i<=c:
+        print(a,'x',i,'=',i*a)
+        i += 1
+    print()
+    a += 1
+
+    
+    
